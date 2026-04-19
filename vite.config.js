a89fe667abeaa2329bf5e323367d8d1af90b982d
Eduardo <evelezng@gmail.com>
2026-04-19 15:09:21 -0400

    Github pages setup

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/receiptNow_landing/',
   plugins: [react(), tailwindcss()],
 })
